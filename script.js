8b17f8afa26d47ae4119a7c1ed5a68e7077fadbb
Andi <87631838+AndiMahardika@users.noreply.github.com>
2024-08-13 06:09:52 -0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const btnClose = document.querySelector(`.close`);
-const divIklan = document.querySelector(`.iklan`);
-
-btnClose.addEventListener(`click`,function(){
-    divIklan.style.display = "none";
-})
